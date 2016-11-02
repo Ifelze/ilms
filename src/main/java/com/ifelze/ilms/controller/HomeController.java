1410f8e84916f8f324d69059fb89efa2a9eb57cd
nagaifelze <naga@ifelze.com>
2016-11-01 23:30:38 -0400

    Added Home Page

---
+++ src/main/java/com/ifelze/ilms/controller/HomeController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.ifelze.ilms.controller;
+
+import javax.servlet.ServletException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author nithya
+ *
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homePage() throws
+            ServletException {
+    	return "home";
+    }
+}
