e0eed4388ab120172cee9ca93febee6cf5ccbe31
nagaifelze <naga@ifelze.com>
2016-11-01 22:51:37 -0400

    Added Lender, borrowers and teir models, services, controllers.
    Added Login functionality for lender and borrower.

--- src/main/java/com/ifelze/ilms/IlmsApplication.java
+++ src/main/java/com/ifelze/ilms/IlmsApplication.java
@@ -2,9 +2,21 @@ package com.ifelze.ilms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.ifelze.ilms.config.JwtFilter;
 
 @SpringBootApplication
 public class IlmsApplication {
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/ilms/*");
+
+		return registrationBean;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(IlmsApplication.class, args);
 	}
